cbfe3d5b54fb2b1ac5172f09b8b28841d5a3e9f6
owain lewis <owain@owainlewis.com>
2013-04-11 21:10:54 +0100

    using liftM to pull out the IO

--- Main.hs
+++ Main.hs
@@ -1,15 +1,16 @@
-module Nlp
+module Main
 where
 
 import Control.Arrow
 import Control.Monad
 
+import Data.Function(on)
 import Data.Char(isAlpha, toLower)
 
 import System.Directory
 import System.IO
 
-import Data.List(group, partition, sort)
+import Data.List(group, partition, sort, sortBy)
 
 type Freq = (String, Int)
 
@@ -18,11 +19,20 @@ filterCorpus :: [String] -> [[String]]
 filterCorpus = let clean = (map toLower . filter isAlpha) in
   map clean >>> sort >>> group
 
-termFrequency :: [[String]] -> [(String, Int)]
+termFrequency :: [[String]] -> [Freq]
 termFrequency corpus = map (\(x:_) -> (x, (length x))) corpus
+
+sortedFrequency :: [Freq] -> [Freq]
+sortedFrequency f = sortBy (compare `on` snd) f
+
+sorted :: FilePath -> IO [Freq]
+sorted file = sortedFrequency `liftM` tokenize file
 
 -- | Given a document tokenize >> filter >> return frequency
 tokenize document = do
   content <- liftM words $ readFile document
   let freq = termFrequency $ filterCorpus content
   return freq
+
+{- Main -}
+
